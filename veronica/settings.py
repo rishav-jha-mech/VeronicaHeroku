1dd32f15c3e6de6db51052663215bee01aa65062
rishav-jha-mech <rishav.rj74@gmail.com>
2021-06-19 00:57:11 +0530

    first commit

--- veronica/settings.py
+++ veronica/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import django_heroku # This is for HEROKU OBVIOUSLY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# THIS IS ALSO FOR HEROKU ONLY
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
